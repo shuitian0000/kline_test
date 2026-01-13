45baddd65c7ea4f7b76eecd1d88d929d598aedb1
Not Found <wangyongwei_2003@126.com>
2026-01-13 16:57:17 +0800

    Update launch log message in app.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 App({
   onLaunch() {
     // Initialize global data or perform startup logic
-    console.log('kline_test app launched');
+    //console.log('kline_test app launched');
+    console.log("人生K线小程序启动");
   },
   globalData: {}
 });
