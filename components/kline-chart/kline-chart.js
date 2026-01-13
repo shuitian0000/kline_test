8d751c16e58f105d4e10f03a3d8077c865b3933c
Not Found <wangyongwei_2003@126.com>
2026-01-13 16:11:19 +0800

    Update kline-chart.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- components/kline-chart/kline-chart.js
+++ components/kline-chart/kline-chart.js
@@ -1,26 +1,14 @@
 // components/kline-chart/kline-chart.js
 Component({
-  properties: {
-    data: {
-      type: Array,
-      value: []
-    }
-  },
-  data: {},
-  lifetimes: {
-    attached() {
-      // Initialize chart rendering
-      this.init();
-    }
-  },
+  properties: { klineData: Array },
+  data: { candleRects: [] },
+  lifetimes: { ready() { this.draw() } },
+  observers: { klineData() { this.draw() } },
+
   methods: {
-    init() {
-      // Placeholder: set up canvas drawing / chart library
-      console.log('kline-chart initialized');
-    },
-    render(data) {
-      // Placeholder: render kline using data
-      console.log('render kline with', data);
-    }
+    draw() { /* Canvas 绘制逻辑，参考之前的 X-3 */ },
+    renderKLine(ctx, w, h) { /* 计算 K 线坐标，填充色 */ },
+    onCanvasTap(e) { /* 点击命中逻辑 */ }
   }
-});
+})
+
