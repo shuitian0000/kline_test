6efddb5bc932db9d425cd2bcd53e71913f84c161
Not Found <wangyongwei_2003@126.com>
2026-01-13 16:16:22 +0800

    Update plan.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/access/plan.js
+++ utils/access/plan.js
@@ -1,7 +1,3 @@
 // utils/access/plan.js
-// Access plan utilities / feature gating
-function hasPlan(user, planName) {
-  // placeholder: determine if user has plan
-  return (user && user.plan === planName);
-}
-module.exports = { hasPlan };
+export const PLAN={FREE:'free',PLUS:'plus',PRO:'pro'}
+
