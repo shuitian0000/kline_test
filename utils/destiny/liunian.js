25316c02aeae76c32cbdefc79a30acb0d6313ab9
Not Found <wangyongwei_2003@126.com>
2026-01-14 11:28:06 +0800

    Update liunian.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/destiny/liunian.js
+++ utils/destiny/liunian.js
@@ -1,1 +1,13 @@
+//流年引擎（每一年）
+import { ganzhiFromIndex } from '../bazi/ganzhi';
+import { GAN_WUXING } from '../bazi/wuxing-map';
 
+export function getLiuNian(year) {
+  const idx = year - 4;
+  const pillar = ganzhiFromIndex(idx);
+  return {
+    year,
+    pillar,
+    wuxing: GAN_WUXING[pillar[0]]
+  };
+}
