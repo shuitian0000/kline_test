5c48830b4e165b785ea4fcb9dfba944c44018335
Not Found <wangyongwei_2003@126.com>
2026-01-14 11:15:17 +0800

    Update true-solar-time.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/calendar/true-solar-time.js
+++ utils/calendar/true-solar-time.js
@@ -1,1 +1,6 @@
+// 经度 → 真太阳时修正
+export function toTrueSolarTime(date, longitude) {
+  const offsetMinutes = longitude * 4; // 每度 4 分钟
+  return new Date(date.getTime() + offsetMinutes * 60000);
+}
 
