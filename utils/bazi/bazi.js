e1b38bdc88067dc325c978a4eec6a4b51481f8eb
Not Found <wangyongwei_2003@126.com>
2026-01-15 18:26:28 +0800

    Update bazi.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/bazi/bazi.js
+++ utils/bazi/bazi.js
@@ -1,3 +1,10 @@
 
 // 八字计算示例函数
 export function calcBazi(user){return {shishen:'正官',score:1.0}}
+
+// {
+//   year: { tg: '甲', dz: '子' },
+//   month:{ tg: '丙', dz: '寅' },
+//   day:  { tg: '戊', dz: '午' },   // 日主
+//   hour: { tg: '庚', dz: '申' }
+// }
