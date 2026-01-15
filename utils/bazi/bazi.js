fd72061049d40031d2857224ce96746e32031949
Not Found <wangyongwei_2003@126.com>
2026-01-15 18:27:24 +0800

    Update bazi.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/bazi/bazi.js
+++ utils/bazi/bazi.js
@@ -8,3 +8,12 @@ export function calcBazi(user){return {shishen:'正官',score:1.0}}
 //   day:  { tg: '戊', dz: '午' },   // 日主
 //   hour: { tg: '庚', dz: '申' }
 // }
+
+// const WUXING_VALUE = {
+//   木: 1.0,
+//   火: 1.1,
+//   土: 1.0,
+//   金: 0.9,
+//   水: 0.95
+// }
+
