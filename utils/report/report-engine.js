73c504c05714a7787443732782ee173dc2ca94c3
Not Found <wangyongwei_2003@126.com>
2026-01-15 17:16:43 +0800

    Update report-engine.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/report/report-engine.js
+++ utils/report/report-engine.js
@@ -1,1 +1,19 @@
 // 年度报告生成引擎占位
+
+import { REPORT_STRUCTURE } from './report-structure'
+import * as sections from './report-sections'
+
+export function buildAnnualReport(input) {
+  const content = {}
+
+  REPORT_STRUCTURE.forEach(sec => {
+    content[sec.key] = sections[`build${capitalize(sec.key)}`](input)
+  })
+
+  return {
+    year: input.year,
+    content
+  }
+}
+
+function capitalize(s){ return s.charAt(0).toUpperCase()+s.slice(1) }
