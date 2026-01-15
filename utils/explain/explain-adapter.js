47b14c5f777e8d8b681edea6c77cab13b4d34249
Not Found <wangyongwei_2003@126.com>
2026-01-15 18:23:38 +0800

    Update explain-adapter.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/explain/explain-adapter.js
+++ utils/explain/explain-adapter.js
@@ -1,6 +1,10 @@
 // 页面调用入口
 // Explain Adapter（页面只调用这一层）
 
+// export async function explain(p){
+//   return `${p.year}年走势${p.value>0?'上行':'回调'}`
+// }
+
 import { buildExplain } from './explain-engine'
 
 export async function explain(point) {
