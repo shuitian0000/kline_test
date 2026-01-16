a3b5c6df0cf63ca6f0853cf5259e3b4a97aa9912
Not Found <wangyongwei_2003@126.com>
2026-01-16 11:18:38 +0800

    Update wuxing.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

--- utils/bazi-1/wuxing.js
+++ utils/bazi-1/wuxing.js
@@ -1,14 +1,24 @@
 // wuxing.js（五行平衡）
 
-import { GAN_WUXING, ZHI_WUXING } from './ganzhi'
+// import { GAN_WUXING, ZHI_WUXING } from './ganzhi'
 
-export function calcWuXingBalance(bazi) {
-  const count = { 木:0, 火:0, 土:0, 金:0, 水:0 }
+// export function calcWuXingBalance(bazi) {
+//   const count = { 木:0, 火:0, 土:0, 金:0, 水:0 }
 
-  Object.values(bazi).forEach(col => {
-    if (col.gan) count[GAN_WUXING[col.gan]]++
-    if (col.zhi) count[ZHI_WUXING[col.zhi]]++
-  })
+//   Object.values(bazi).forEach(col => {
+//     if (col.gan) count[GAN_WUXING[col.gan]]++
+//     if (col.zhi) count[ZHI_WUXING[col.zhi]]++
+//   })
 
-  return count
+//   return count
+// }
+
+// wuxing.js（五行关系，冻结）
+
+export function calcWuxing(dayPillar) {
+  return {
+    gan: dayPillar.gan.wuxing,
+    zhi: dayPillar.zhi.wuxing
+  }
 }
+
