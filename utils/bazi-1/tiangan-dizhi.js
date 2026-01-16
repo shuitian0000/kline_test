d93a48e645d42fefcfac91e6777f0d43cc5b7afd
Not Found <wangyongwei_2003@126.com>
2026-01-16 11:17:10 +0800

    Create tiangan-dizhi.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

---
+++ utils/bazi-1/tiangan-dizhi.js
@@ -0,0 +1,29 @@
+// tiangan-dizhi.js（基础常量，冻结）
+
+export const TIANGAN = [
+  { name: '甲', wuxing: 'wood', yinYang: 'yang' },
+  { name: '乙', wuxing: 'wood', yinYang: 'yin' },
+  { name: '丙', wuxing: 'fire', yinYang: 'yang' },
+  { name: '丁', wuxing: 'fire', yinYang: 'yin' },
+  { name: '戊', wuxing: 'earth', yinYang: 'yang' },
+  { name: '己', wuxing: 'earth', yinYang: 'yin' },
+  { name: '庚', wuxing: 'metal', yinYang: 'yang' },
+  { name: '辛', wuxing: 'metal', yinYang: 'yin' },
+  { name: '壬', wuxing: 'water', yinYang: 'yang' },
+  { name: '癸', wuxing: 'water', yinYang: 'yin' }
+]
+
+export const DIZHI = [
+  { name: '子', wuxing: 'water' },
+  { name: '丑', wuxing: 'earth' },
+  { name: '寅', wuxing: 'wood' },
+  { name: '卯', wuxing: 'wood' },
+  { name: '辰', wuxing: 'earth' },
+  { name: '巳', wuxing: 'fire' },
+  { name: '午', wuxing: 'fire' },
+  { name: '未', wuxing: 'earth' },
+  { name: '申', wuxing: 'metal' },
+  { name: '酉', wuxing: 'metal' },
+  { name: '戌', wuxing: 'earth' },
+  { name: '亥', wuxing: 'water' }
+]
