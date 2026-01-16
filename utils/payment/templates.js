50397a46f7817681f908931a6a4c794f2396f241
Not Found <wangyongwei_2003@126.com>
2026-01-16 14:13:14 +0800

    Create templates.js
    Signed-off-by: Not Found <wangyongwei_2003@126.com>

---
+++ utils/payment/templates.js
@@ -0,0 +1,10 @@
+export const TEMPLATES = {
+  evolveDetail: {
+    text: '高级进化分析需要开通会员才能查看。',
+    advice: '开通会员即可查看详细成长趋势和策略建议。'
+  },
+  fullscreen: {
+    text: '全屏 K 线视图为会员专享功能。',
+    advice: '开通会员即可解锁全屏模式和高级交互。'
+  }
+}
